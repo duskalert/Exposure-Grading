c00416a644176e7184f2766e44762f56e35aa748
developer <developer@example.com>
2026-07-10 13:49:45 +0800

    添加评审台方块模型、纹理、blockstate 和 item 模型

---
+++ src/main/java/com/example/exposuregrading/ExposureGrading.java
@@ -0,0 +1,12 @@
+package com.example.exposuregrading;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ExposureGrading.MODID)
+public class ExposureGrading {
+    public static final String MODID = "exposuregrading";
+
+    public ExposureGrading(IEventBus modBus) {
+    }
+}
